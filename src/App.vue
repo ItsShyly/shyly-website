412b2edd7c683f76cf3cb86b47e050c4cef75595
Shyly <shyly>
2024-11-06 23:25:26 +0100

    Initial vue setup

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Navbar from "@/components/common/navbar.vue";
+
+</script>
+
+<template>
+  <header>
+    <Navbar brand="shyly"/>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+</style>
