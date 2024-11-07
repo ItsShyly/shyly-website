b2f24be0a50548cc94c8e0e7be8cdb2d7f439584
Shyly <shyly>
2024-11-07 05:10:22 +0100

    implement basic routing structure

---
+++ src/components/common/GlobalMenu.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <div>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/example1">Example 1</RouterLink>
+    <RouterLink to="/example2">Example 2</RouterLink>
+    <RouterLink to="/example3">Example 3</RouterLink>
+    <RouterLink to="/example4">Example 4</RouterLink>
+  </div>
+</template>
+
+<style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
-import '@/assets/scss/main.scss'
+// src/main.ts
 
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from '@/App.vue'
+import router from '@/router'
 
-createApp(App).mount('#app')
+import '@/assets/scss/main.scss'
+
+createApp(App).use(router).mount('#app')
